45894bbd6fa83d438ca2aeca3ad7776bc4013436
shinyehwan <jghisjl007@naver.com>
2023-05-25 01:19:26 +0900

    jwt 적용

--- src/main/java/com/example/demo/src/company/CompanyController.java
+++ src/main/java/com/example/demo/src/company/CompanyController.java
@@ -1,5 +1,7 @@
 package com.example.demo.src.company;
 
+import static com.example.demo.config.BaseResponseStatus.*;
+
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,11 +14,10 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.demo.config.BaseException;
 import com.example.demo.config.BaseResponse;
 import com.example.demo.src.company.model.GetCompanyRes;
-import com.example.demo.src.user.model.GetUserRes;
 import com.example.demo.utils.JwtService;
 
 @RestController
-@RequestMapping("/app/company")
+@RequestMapping("/app/users")
 public class CompanyController {
 	@Autowired
 	private CompanyService companyService;
@@ -29,10 +30,17 @@ public class CompanyController {
 	}
 
 	@ResponseBody
-	@GetMapping("/{companyIdx}")
+	@GetMapping("/{userIdx}/{companyIdx}")
 	public BaseResponse<List<GetCompanyRes>> getCompanyInfo(
+		@PathVariable("userIdx") Integer userIdx,
 		@PathVariable("companyIdx") Integer companyIdx) {
 		try {
+			//jwt에서 idx 추출.
+			int userIdxByJwt = jwtService.getUserIdx();
+			//userIdx와 접근한 유저가 같은지 확인
+			if( userIdx != userIdxByJwt){
+				return new BaseResponse<>(INVALID_USER_JWT);
+			}
 			List<GetCompanyRes> getCompanyRes = companyService.getCompany(companyIdx);
 			return new BaseResponse<>(getCompanyRes);
 		} catch (BaseException exception) {
