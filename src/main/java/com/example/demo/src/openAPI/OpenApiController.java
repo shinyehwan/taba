0e447e363f7241f9df365b54eeb4baf3f7edbda7
shinyehwan <jghisjl007@naver.com>
2023-06-15 23:28:54 +0900

    openAPI 수정

--- src/main/java/com/example/demo/src/openAPI/OpenApiController.java
+++ src/main/java/com/example/demo/src/openAPI/OpenApiController.java
@@ -1,6 +1,7 @@
 package com.example.demo.src.openAPI;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ public class OpenApiController {
 	@Autowired
 	private OpenApiService openApiService;
 
+	// @CrossOrigin(origins = "https://localhost:3000")
 	@GetMapping("/openapi")
 	public String getOpenApiData() {
 		return openApiService.callOpenApi();
